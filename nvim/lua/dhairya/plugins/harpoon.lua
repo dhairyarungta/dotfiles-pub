5c315a3efc45805d35e49457d2da6253a93045f7
Dhairya <97079960+dhairyarungta@users.noreply.github.com>
2025-06-28 07:58:27 +0000

    Change harpoon keymaps

--- nvim/lua/dhairya/plugins/harpoon.lua
+++ nvim/lua/dhairya/plugins/harpoon.lua
@@ -21,9 +21,9 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     
     vim.keymap.set("n", "<C-g>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-d>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-a>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-a>", function() harpoon:list():select(3) end)
+    -- vim.keymap.set("n", "<C->", function() harpoon:list():select(4) end)
     
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<leader>hj", function() harpoon:list():prev() end, 
